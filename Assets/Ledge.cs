01cf5f0845731f6d211915ff14ea59225e47421f
Paulino Marié <paulinodjm@hotmail.com>
2015-03-15 16:57:01 +0100

    Ajoute le collider des rebords

--- Assets/Ledge.cs
+++ Assets/Ledge.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Ledge : MonoBehaviour 
+public class Ledge : MonoBehaviour
 {
+    #region Gizmos parameters
+
     private static readonly Color _color = new Color(Color.cyan.r, Color.cyan.g, Color.cyan.b, 0.5F);
 
     private const float _borderSize = 0.05F;
@@ -10,7 +11,11 @@ public class Ledge : MonoBehaviour
     private const float _width = _borderSize * 2F;
 
     private const float _height = 0.2F;
-         
+
+    #endregion
+
+    private const float _colliderRadius = 1.0F;
+
     void OnDrawGizmos()
     {
         Gizmos.color = _color;
@@ -22,5 +27,30 @@ public class Ledge : MonoBehaviour
         var size = new Vector3(transform.lossyScale.x, _height, _width);
 
         Gizmos.DrawCube(center, size);
+    }
+
+    void Awake()
+    {
+        SpawnCapsule();
+    }
+
+    /// <summary>
+    /// Creates the capsule used for the ledge detection.
+    /// </summary>
+    private void SpawnCapsule()
+    {
+        var capsuleGameObject = new GameObject("Trigger");
+
+        capsuleGameObject.transform.position = transform.position;
+        capsuleGameObject.transform.rotation = transform.rotation;
+
+        var capsule = capsuleGameObject.AddComponent<CapsuleCollider>();
+        capsule.radius = _colliderRadius;
+        capsule.direction = 0;
+        capsule.height = transform.lossyScale.x + capsule.radius * 2F;
+        capsule.center = new Vector3(transform.lossyScale.x / 2F, 0F, 0F);
+        capsule.isTrigger = true;
+
+        capsuleGameObject.transform.parent = transform;
     }
 }
