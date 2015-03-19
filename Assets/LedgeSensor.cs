3643e4fd2a76909307eb9e8770364b373553694d
Marié Paulino <paulinodjm@hotmail.com>
2015-03-19 22:11:52 +0100

    Ajoute le calcul des position atteignables par un rebord à proximité

--- Assets/LedgeSensor.cs
+++ Assets/LedgeSensor.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class LedgeSensor : MonoBehaviour 
+public class LedgeSensor : MonoBehaviour
 {
     /// <summary>
     /// Returns the ledges near the character.
@@ -18,7 +18,17 @@ public class LedgeSensor : MonoBehaviour
     /// </summary>
     public List<Ledge> LowerLedges { get; private set; }
 
+    /// <summary>
+    /// Returns the positions the character can reach using the legdes.
+    /// </summary>
+    public List<Vector3> TargetPositions { get; private set; }
+
     private CharacterController _characterController;
+
+    public LedgeSensor()
+    {
+        TargetPositions = new List<Vector3>();
+    }
 
     void Awake()
     {
@@ -44,10 +54,24 @@ public class LedgeSensor : MonoBehaviour
         Ledges.Remove(ledge);
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = Matrix4x4.identity;
+        Gizmos.color = new Color(1F, 0F, 0F, 0.5F);
+
+        foreach (var position in TargetPositions)
+        {
+            var drawPosition = position;
+            drawPosition.y += _characterController.radius;
+            Gizmos.DrawSphere(drawPosition, _characterController.radius);
+        }
+    }
+
     void Update()
     {
         UpperLedges.Clear();
         LowerLedges.Clear();
+        TargetPositions.Clear();
 
         foreach (var ledge in Ledges)
         {
@@ -59,6 +83,69 @@ public class LedgeSensor : MonoBehaviour
             {
                 UpperLedges.Add(ledge);
             }
+
+            Vector3 vector, grabPosition;
+
+            //
+            // délimite le rebord
+            //
+            var ledgeStart = ledge.transform.position;
+            var ledgeEnd = ledgeStart + (ledge.transform.right * ledge.transform.lossyScale.x);
+
+            //
+            // calcule le point d'accroche
+            //
+            // <- à hauteur du joueur
+            vector = ledge.transform.position - transform.position;
+            grabPosition = Vector3.Project(vector, ledge.transform.forward) + transform.position;
+
+            // <- à hauteur du rebord
+            vector = grabPosition - ledge.transform.position;
+            grabPosition = Vector3.Project(vector, ledge.transform.right) + ledge.transform.position;
+
+            var playerPosition = transform.position;
+            playerPosition.y = grabPosition.y;
+
+            //
+            // valide le point d'accroche
+            //
+            bool grabPositionOk;
+
+            if ((grabPosition - ledgeStart).normalized == -ledge.transform.right)
+            {
+                grabPositionOk = false;
+            }
+            else
+            {
+                var distanceFromLedgeStart = (grabPosition - ledgeStart).magnitude;
+                grabPositionOk = distanceFromLedgeStart <= ledge.transform.lossyScale.x;
+            }
+            if (!grabPositionOk)
+                continue;
+
+            //
+            // gère les extrémités de rebords (attention aux pentes!)
+            //
+            Vector3 safeGrabPosition;
+            if (ledge.transform.lossyScale.x <= (_characterController.radius * 2))
+            {
+                safeGrabPosition = ledgeStart + (ledge.transform.right * _characterController.radius);
+            }
+            else if ((grabPosition - ledgeStart).magnitude < _characterController.radius)
+            {
+                safeGrabPosition = ledgeStart + (ledge.transform.right * _characterController.radius);
+            }
+            else if ((grabPosition - ledgeEnd).magnitude < _characterController.radius)
+            {
+                safeGrabPosition = ledgeEnd - (ledge.transform.right * _characterController.radius);
+            }
+            else
+            {
+                safeGrabPosition = grabPosition;
+            }
+
+            var nextPosition = safeGrabPosition + (grabPosition - playerPosition).normalized * _characterController.radius;
+            TargetPositions.Add(nextPosition);
         }
     }
 }
