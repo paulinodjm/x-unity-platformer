affe447f87e332fdaad6fef5d0295955d5f6e69c
Paulino Marié <paulinodjm@hotmail.com>
2015-04-26 15:10:38 +0200

    Ajoute un début de gestion des directions, ainsi que les animation de départ latéral

--- Assets/MotionController.cs
+++ Assets/MotionController.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 [RequireComponent(typeof(Animator))]
-public class MotionController : MonoBehaviour 
+public class MotionController : MonoBehaviour
 {
     private Animator _animator;
+
+    private float _lastDirection;
 
     void Awake()
     {
@@ -13,6 +15,19 @@ public class MotionController : MonoBehaviour
 
     void Update()
     {
-        _animator.SetBool("Walk", Input.GetAxis("Vertical") > 0F);
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0F, Input.GetAxis("Vertical")).normalized;
+        var direction = move.magnitude > 0F ? Mathf.DeltaAngle(transform.rotation.eulerAngles.y, Quaternion.LookRotation(move).eulerAngles.y) : _lastDirection;
+        _lastDirection = direction;
+
+        if (move.magnitude > 0F)
+        {
+            var rotationRate = _animator.GetFloat("RotationRate") * .02F;
+            print(rotationRate);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move), rotationRate);
+        }
+
+        _animator.SetBool("Walk", move.magnitude > 0F);
+        _animator.SetFloat("Direction", direction, .05F, Time.deltaTime);
+        
     }
 }
