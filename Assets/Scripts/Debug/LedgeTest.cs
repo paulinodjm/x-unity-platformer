dc20b4f6cd55c3c28375fb748ba26495ad31b148
Paulino <paulinodjm@hotmail.com>
2015-08-11 16:21:36 +0200

    Simplifie le ledgeSensor pour n'effectuer que la détection des rebords à proximité

---
+++ Assets/Scripts/Debug/LedgeTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LedgeTest : MonoBehaviour
+{
+    public Color LedgeColor = Color.black;
+    
+    private LedgeSensor _ledgeSensor;
+
+    protected void Start()
+    {
+        _ledgeSensor = GetComponent<LedgeSensor>();
+    }
+
+    protected void OnDrawGizmosSelected()
+    {
+        if (_ledgeSensor == null)
+            return;
+
+        Gizmos.color = LedgeColor;
+        Gizmos.matrix = Matrix4x4.identity;
+
+        foreach (var ledge in _ledgeSensor.Ledges)
+        {
+            Gizmos.DrawLine(ledge.Start, ledge.End);
+        }
+    }
+}
---
+++ Assets/Scripts/LedgeSensor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects and keep an eye on the ledges arount the character
+/// </summary>
+public class LedgeSensor : MonoBehaviour
+{
+    /// <summary>
+    /// Returns the ledges near the character.
+    /// </summary>
+    public List<Ledge> Ledges { get; private set; }
+
+    protected void Start()
+    {
+        Ledges = new List<Ledge>();
+    }
+
+    protected void OnTriggerEnter(Collider collider)
+    {
+        var ledge = collider.gameObject.GetComponentInParent<Ledge>();
+        if (ledge == null) return;
+
+        Ledges.Add(ledge);
+    }
+
+    protected void OnTriggerExit(Collider collider)
+    {
+        var ledge = collider.gameObject.GetComponentInParent<Ledge>();
+        if (ledge == null) return;
+
+        Ledges.Remove(ledge);
+    }
+}
