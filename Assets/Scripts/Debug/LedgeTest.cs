1fc526801cffa8686278d7dff045836f08880850
Paulino <paulinodjm@hotmail.com>
2015-08-12 11:23:56 +0200

    Ajoute la différenciation entre les rebords à grimper et ceux à descendre, lors de l'affichage

--- Assets/Scripts/Debug/LedgeTest.cs
+++ Assets/Scripts/Debug/LedgeTest.cs
@@ -4,7 +4,8 @@ using Common;
 
 public class LedgeTest : MonoBehaviour
 {
-    public Color LedgeColor = Color.black;
+    public Color LowerLedgeColor = Color.black;
+    public Color UpperLedgeColor = Color.red;
     
     private LedgeSensor _ledgeSensor;
     private ICharacterProperties _character;
@@ -20,15 +21,18 @@ public class LedgeTest : MonoBehaviour
         if (_ledgeSensor == null)
             return;
 
-        Gizmos.color = LedgeColor;
         Gizmos.matrix = Matrix4x4.identity;
 
         foreach (var ledge in _ledgeSensor.Ledges)
         {
-            Gizmos.DrawLine(ledge.Start, ledge.End);
-
             var grabPosition = ledge.CalcGrabPosition(transform.position, _character.Radius);
+
+            Gizmos.color = (grabPosition.Position.y < transform.position.y + _character.StepOffset) ? LowerLedgeColor : UpperLedgeColor;
+
+            Gizmos.DrawLine(ledge.Start, ledge.End);
             Gizmos.DrawSphere(grabPosition.Position, .1F);
         }
+
+        print(_ledgeSensor.Ledges.Count);
     }
 }
